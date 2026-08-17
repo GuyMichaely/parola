697284cf3721b23581f72f3b08ef01470aaaf7c7
GuyMichaely <guymichaely@gmail.com>
2026-08-17 00:14:47 -0400

    Add storage contract module

---
+++ web/src/storage/types.ts
@@ -0,0 +1,9 @@
+import type { Flashcard } from "../cards/types";
+
+export interface CardStorage {
+  readonly label: string;
+  listCards(): Promise<Flashcard[]>;
+  createCards(cards: Flashcard[]): Promise<Flashcard[]>;
+  updateCard(card: Flashcard): Promise<Flashcard>;
+  deleteCard(id: number): Promise<void>;
+}
