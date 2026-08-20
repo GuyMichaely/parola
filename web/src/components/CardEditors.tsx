4aaea2987d48dcf69421408012aede3673d683f1
GuyMichaely <guymichaely@gmail.com>
2026-08-19 21:36:08 -0400

    Remove deck editor exports

--- web/src/components/CardEditors.tsx
+++ web/src/components/CardEditors.tsx
@@ -2,4 +2,4 @@ export { AddCardModal } from "./AddCardModal";
 export { BulkEditCardsModal } from "./BulkEditCardsModal";
 export { EditCardModal } from "./EditCardModal";
 export { InventoryCardsEditor } from "./InventoryCardsEditor";
-export { deckName, deckTagPrefix, localDateStamp, visibleTags } from "../cards/editorModel";
+export { localDateStamp } from "../cards/editorModel";
