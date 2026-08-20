267211cd540800bf05c6d4da641265826cc7370c
GuyMichaely <guymichaely@gmail.com>
2026-08-19 23:59:08 -0400

    Remove focused bulk editor export

--- web/src/components/CardEditors.tsx
+++ web/src/components/CardEditors.tsx
@@ -1,5 +1,4 @@
 export { AddCardModal } from "./AddCardModal";
-export { BulkEditCardsModal } from "./BulkEditCardsModal";
 export { EditCardModal } from "./EditCardModal";
 export { InventoryCardsEditor } from "./InventoryCardsEditor";
 export { localDateStamp } from "../cards/editorModel";
